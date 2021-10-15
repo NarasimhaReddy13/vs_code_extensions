9b2b2262dfcbbc7480fd93428f4afaff53c3ebc8
“NarasimhaReddy13” <narasimhareddy.a444@gmail.com>
2021-10-15 14:53:56 +0530

    Initial Commit for VS Code extensions

---
+++ Theory.js
@@ -0,0 +1,30 @@
+const name1 = 'Nara';
+
+function myname() {
+	console.log(name1);
+}
+
+
+
+
+console.log('Hello');  // type  clg and  enter
+
+
+console.error('Error') // cer
+
+
+
+
+const arr = [1,2,3];
+
+arr.forEach(item => {
+    console.log(item);
+
+    if (item === 2) {
+        console.log("Nara");
+    }
+})
+// fre --- will give forEach
+
+
+
